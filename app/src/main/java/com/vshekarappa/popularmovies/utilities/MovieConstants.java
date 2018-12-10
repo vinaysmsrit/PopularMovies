483c35d3c6e42a1f9a9486382c7bb5fef135f598
vinaysmsrit <vinaysmsrit@gmail.com>
2018-12-09 23:39:34 -0600

    API Key added

--- app/src/main/java/com/vshekarappa/popularmovies/utilities/MovieConstants.java
+++ app/src/main/java/com/vshekarappa/popularmovies/utilities/MovieConstants.java
@@ -4,7 +4,7 @@ public class MovieConstants {
 
     public static final String APP_LOG_TAG = "POPULAR_MOVIES";
 
-    public static final String API_KEY = "";
+    public static final String API_KEY = "5c25eaa5f74d80e33ae9df32e7b1ff55";
 
     public static final String MOVIEDB_BASE_URL = "http://api.themoviedb.org/3/movie";
     public static final String MOVIE_DETAIL_EXTRA = "movie_detail";
